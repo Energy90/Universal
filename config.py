5f2b89beb78e5f6632e16b6dbd40ceb8327f420a
Energy90 <mcmalindi@gmail.com>
2020-06-19 19:56:03 +0200

    updating gitignore file

--- config.py
+++ config.py
@@ -4,5 +4,8 @@ class Config(object):
     # the secrete key
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'cc6e2a7a076e4989a4171f132bd897cd'
 
+    SIJAX_STATIC_PATH = 'app/static/js/sijax'
+    SIJAX_JSON_URI = 'app/static/js/sijax/json2.js'
+
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
     
